7756cc2d09ae41f71f0ce21935dda467eab0caa5
august-2004 <115717134+august-2004@users.noreply.github.com>
2025-02-01 19:33:27 +0530

    Add sorting functionality to Table component with arrow indicators

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,17 +1,81 @@
 import TableRow from "./TableRow";
 import "./Table.css";
-export default function Table({ data }) {
+
+export default function Table({ data, sortBy, sortOrder }) {
+	const handleSort = (column) => {
+		const order =
+			sortOrder.column === column && sortOrder.order === "asc" ? "desc" : "asc";
+		sortBy(column, order);
+	};
+
 	return (
 		<div>
 			<table>
 				<thead>
 					<tr className="table-header">
-						<th>ID</th>
+						<th>
+							ID
+							<button onClick={() => handleSort("id")}>
+								{sortOrder.column === "id" && sortOrder.order === "asc" ? (
+									<img src={`up-arrow.svg`} width="10" height="10" alt="up" />
+								) : (
+									<img
+										src={`down-arrow.svg`}
+										width="10"
+										height="10"
+										alt="down"
+									/>
+								)}
+							</button>
+						</th>
 						<th>Title</th>
 						<th>Description</th>
-						<th>Category</th>
-						<th>Price</th>
-						<th>Rating</th>
+						<th>
+							Category
+							<button onClick={() => handleSort("category")}>
+								{sortOrder.column === "category" &&
+								sortOrder.order === "asc" ? (
+									<img src={`up-arrow.svg`} width="10" height="10" alt="up" />
+								) : (
+									<img
+										src={`down-arrow.svg`}
+										width="10"
+										height="10"
+										alt="down"
+									/>
+								)}
+							</button>
+						</th>
+						<th>
+							Price
+							<button onClick={() => handleSort("price")}>
+								{sortOrder.column === "price" && sortOrder.order === "asc" ? (
+									<img src={`up-arrow.svg`} width="10" height="10" alt="up" />
+								) : (
+									<img
+										src={`down-arrow.svg`}
+										width="10"
+										height="10"
+										alt="down"
+									/>
+								)}
+							</button>
+						</th>
+						<th>
+							Rating
+							<button onClick={() => handleSort("rating")}>
+								{sortOrder.column === "rating" && sortOrder.order === "asc" ? (
+									<img src={`up-arrow.svg`} width="10" height="10" alt="up" />
+								) : (
+									<img
+										src={`down-arrow.svg`}
+										width="10"
+										height="10"
+										alt="down"
+									/>
+								)}
+							</button>
+						</th>
 					</tr>
 				</thead>
 				<tbody>
