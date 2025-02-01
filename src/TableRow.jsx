824c7954ad139420783f652bb12dab9978a31db1
august-2004 <115717134+august-2004@users.noreply.github.com>
2025-02-01 17:40:01 +0530

    Add category logo to TableRow component

--- src/TableRow.jsx
+++ src/TableRow.jsx
@@ -4,7 +4,15 @@ export default function TableRow({ product }) {
 			<td>{product.id}</td>
 			<td>{product.title}</td>
 			<td>{product.description}</td>
-			<td>{product.category}</td>
+			<td>
+				{product.category}
+				<img
+					src={`${product.category}.svg`}
+					alt={`${product.category} logo`}
+					width="20"
+					height="20"
+				/>
+			</td>
 			<td>{product.price}</td>
 			<td>{product.rating}</td>
 		</tr>
