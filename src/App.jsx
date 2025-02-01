1e6c20ffc821054401bd92b4c13505f99d4d670a
august-2004 <115717134+august-2004@users.noreply.github.com>
2025-02-01 19:13:59 +0530

    sorting functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Table from "./Table";
 import "./App.css";
 
 function App() {
-	const [data, setData] = useState([]);
-
+	const [sortedData, setSortedData] = useState([]);
 	useEffect(() => {
 		fetchData();
 	}, []);
@@ -12,12 +11,26 @@ function App() {
 	const fetchData = async () => {
 		const response = await fetch("https://dummyjson.com/products");
 		const fetchedData = await response.json();
-		setData(fetchedData.products);
+		setSortedData(fetchedData.products);
+		sortBy("id");
 	};
-	if (data.length === 0) {
+	const sortBy = (column, order = "asc") => {
+		const sorted = [...sortedData].sort((a, b) => {
+			if (a[column] < b[column]) {
+				return order === "asc" ? -1 : 1;
+			}
+			if (a[column] > b[column]) {
+				return order === "asc" ? 1 : -1;
+			}
+			return 0;
+		});
+		setSortedData(sorted);
+	};
+
+	if (sortedData.length === 0) {
 		return <div>Fetching...</div>;
 	}
-	return <Table data={data} />;
+	return <Table data={sortedData} sortBy={sortBy} />;
 }
 
 export default App;
